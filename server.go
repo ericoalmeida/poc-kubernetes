20f961582a1b3bb6270044aed2e707aee5551a5c
Erico Almeida <erico.silva@invillia.com>
2022-05-03 03:20:20 -0400

    Add simple go app

---
+++ server.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", Hello)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1> Hello world! </h1>"))
+}
