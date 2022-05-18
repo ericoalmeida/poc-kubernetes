062f3b30b0d90e858ca63b2f43e1e4ce7e69c276
Erico Almeida <ericoalmeidamvp@gmail.com>
2022-05-18 04:21:45 -0400

    Add endpoint to load config file

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -9,6 +10,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/load-config", LoadConfig)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
@@ -17,4 +19,14 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	USER_NAME := os.Getenv("USER_NAME")
 
 	fmt.Fprintf(w, "<h1> Hello %s!!! <h1>", USER_NAME)
+}
+
+func LoadConfig(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("/go/test/.env")
+
+	if err != nil {
+		log.Fatal("Error loading file", err)
+	}
+
+	fmt.Fprintf(w, "File data: %s", string(data))
 }
